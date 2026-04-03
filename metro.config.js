5788a354c4291c3febd0fcb35eb0af121ef9947b
vanshitaaa16 <shahvanshita1611@gmail.com>
2026-04-03 15:23:09 +0530

    Changes

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
+// Use the standard Expo Metro config.
+// The previous custom watcher.additionalExclusions option is not supported by EAS
+// and triggered validation warnings during Android builds.
 const config = getDefaultConfig(__dirname);
-
-// Block Metro's resolver AND watcher from accessing the Docker socket
-// which causes EACCES permission errors on Windows
-const exclusionList = [/.*docker-secrets-engine.*/];
-
-config.resolver.blockList = exclusionList;
-
-// Also exclude from the file watcher to prevent the FallbackWatcher crash
-config.watcher = {
-  ...config.watcher,
-  additionalExclusions: exclusionList,
-};
 
 module.exports = config;
